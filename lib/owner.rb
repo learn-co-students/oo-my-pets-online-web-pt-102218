974ac4d33611a9223bd5d02d281f914e3c9222a7
grant smith <grantsmith1256@gmail.com>
2018-11-23 21:36:06 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
+require "pry"
+
 class Owner
-  # code goes here
+  
+  @@instances = []
+ 
+  
+  attr_reader :species
+  attr_accessor :name
+  
+  
+  def initialize(name)
+    @name = name
+    
+    @@instances << self
+  end
+  
+  def species(species)
+    creation = self.new(species)
+    @species = species
+  end
+  
+  def self.count
+    @@instances.size
+  end
+  
+  def self.reset_all
+    @@instances.clear
+  end
+  
+  def self.all
+    @@instances
+  end
+  
+  def self.species
+    @@all_species
+  end
+  
+  
 end
